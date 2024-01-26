791fe688415600ebceca6f82a9380c01ce8fbbd6
Muwawu Moses <mosesmuwawu@gmail.com>
2024-01-26 21:01:57 +0300

    Customize user model with email as the unique identifier

--- management/admin.py
+++ management/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']  # Use 'email' instead of 'username'
+    list_display = ['email', 'is_staff', 'is_active']
+
 admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
