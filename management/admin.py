5296e246f973a2b463ababe06e648887862b4d80
Muwawu Moses <mosesmuwawu@gmail.com>
2024-01-27 07:58:11 +0300

    Added forms and customized user interface

--- management/admin.py
+++ management/admin.py
@@ -2,10 +2,32 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class UserAdmin(BaseUserAdmin):
-    ordering = ['email']  # Use 'email' instead of 'username'
-    list_display = ['email', 'is_staff', 'is_active']
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 admin.site.register(User, UserAdmin)
-admin.site.unregister(Group)
+
+admin.site.site_header = 'HM HOTEL'
+admin.site.site_title = 'HM Hotel Administration'
+admin.site.index_title = 'Welcome to HM Hotel Administration'
