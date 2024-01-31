05d3ddb86a8e0f076a8f110911ab2c391deb74e8
Muwawu Moses <mosesmuwawu@gmail.com>
2024-01-31 23:03:04 +0300

    Added custom * models

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Room
 
-# Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    fields = ('name','capacity','description')
+    list_display = ['name', 'capacity']
+    search_fields = ['name', 'capacity']
+
+admin.site.register(Room, RoomAdmin)
---
+++ hotel/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Room(models.Model):
+    name = models.CharField(
+        max_length=30,
+    )
+    description = models.TextField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
