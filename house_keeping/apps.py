aee2a23db7784f20500080bc72bcf4129960a399
Muwawu Moses <mosesmuwawu@gmail.com>
2024-02-07 20:59:56 +0300

    added signals for updating clean rooms

--- house_keeping/apps.py
+++ house_keeping/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save, pre_delete
 
 
 class HouseKeepingConfig(AppConfig):
@@ -16,4 +16,19 @@ class HouseKeepingConfig(AppConfig):
             signals.delete_previous_task,
             sender='house_keeping.HouseKeepingTask',
             dispatch_uid='delete_previous_task',
+        )
+        post_save.connect(
+            signals.add_room_to_clean_rooms,
+            sender='house_keeping.MaintenanceRequest',
+            dispatch_uid='add_room_to_clean_rooms',
+        )
+        post_save.connect(
+            signals.remove_room_from_clean_rooms,
+            sender='house_keeping.HouseKeepingTask',
+            dispatch_uid='remove_room_from_clean_rooms',
+        )
+        post_save.connect(
+            signals.restore_room_to_clean_rooms,
+            sender='house_keeping.HouseKeepingTask',
+            dispatch_uid='restore_room_to_clean_rooms',
         )
